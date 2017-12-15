613a669d2294954233f20b9725d71037158d702d
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2017-12-15 17:24:00 +0200

    Adaptive activity is created and it works well

---
+++ app/src/main/java/com/idoston/adaptivesystem/Adaptive.java
@@ -0,0 +1,13 @@
+package com.idoston.adaptivesystem;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Adaptive extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_adaptive);
+    }
+}
