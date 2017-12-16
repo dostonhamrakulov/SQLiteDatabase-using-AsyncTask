01d5028f3f6b2e0ebc447ddc1ab98b2aa66dde79
Doston Hamrakulov <doston.hamrakulov@gmail.com>
2017-12-16 05:22:54 +0200

    Several changes in design of Actvities

--- app/src/main/java/com/idoston/adaptivesystem/Adaptive.java
+++ app/src/main/java/com/idoston/adaptivesystem/Adaptive.java
@@ -2,12 +2,32 @@ package com.idoston.adaptivesystem;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Adaptive extends AppCompatActivity {
 
+    EditText take_min_ranking, take_max_ranking;
+    Button get_result;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_adaptive);
+
+        take_min_ranking = (EditText)findViewById(R.id.take_min_ranking);
+        take_max_ranking = (EditText)findViewById(R.id.take_max_ranking);
+
+        get_result = (Button) findViewById(R.id.get_result);
+
+        get_result.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(Adaptive.this, "It is still in process!", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
